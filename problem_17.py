ca87e985c17d816d583af3b535b7c25accea9640
Ryan Foust <r.foust1@outlook.com>
2022-11-22 17:34:35 -0500

    Forgot to account for the instances of 1-9 in each set of 100 (such as 101, 102, 103, etc.)

--- problem_17.py
+++ problem_17.py
@@ -5,40 +5,40 @@ def main():
     total = 0  # keep the running total
 
     # for each 1 found, add another 3 characters
-    total += (3 * 9)  # once for each set of 10 numbers (except for 11), up to 100
+    total += ((3 * 9) * 10)  # once for each set of 10 numbers (except for 11), up to 100. then repeat for each set of 100
     total += (3 * 100)  # once for each number that starts with "one hundred" (100-199)
     total += (3 * 1)  # once for "one thousand" (1000)
 
     # for each 2 found, add another 3 characters
-    total += (3 * 9)  # once for each set of 10 numbers (except for 12), up to 100
+    total += ((3 * 9) * 10)  # once for each set of 10 numbers (except for 12), up to 100. then repeat for each set of 100
     total += (3 * 100)  # once for each number that starts with "two hundred" (200-299)
 
     # for each 3 found, add another 5 characters
-    total += (5 * 9)  # once for each set of 10 numbers (except for 13), up to 100
+    total += ((5 * 9) * 10)  # once for each set of 10 numbers (except for 13), up to 100. then repeat for each set of 100
     total += (5 * 100)  # once for each number that starts with "three hundred" (300-399)
 
     # for each 4 found, add another 4 characters
-    total += (4 * 9)  # once for each set of 10 numbers (except for 14), up to 100
+    total += ((4 * 9) * 10)  # once for each set of 10 numbers (except for 14), up to 100. then repeat for each set of 100
     total += (4 * 100)  # once for each number that starts with "four hundred" (400-499)
 
     # for each 5 found, add another 4 characters
-    total += (4 * 9)  # once for each set of 10 numbers (except for 15), up to 100
+    total += ((4 * 9) * 10)  # once for each set of 10 numbers (except for 15), up to 100. then repeat for each set of 100
     total += (4 * 100)  # once for each number that starts with "five hundred" (500-599)
 
     # for each 6 found (except for 16), add another 3 characters
-    total += (3 * 9)  # once for each set of 10 numbers (except for 16), up to 100
+    total += ((3 * 9) * 10)  # once for each set of 10 numbers (except for 16), up to 100. then repeat for each set of 100
     total += (3 * 100)  # once for each number that starts with "six hundred" (600-699)
 
     # for each 7 found, add another 5 characters
-    total += (5 * 9)  # once for each set of 10 numbers (except for 17), up to 100
+    total += ((5 * 9) * 10)  # once for each set of 10 numbers (except for 17), up to 100. then repeat for each set of 100
     total += (5 * 100)  # once for each number that starts with "seven hundred" (700-799)
 
     # for each 8 found, add another 5 characters
-    total += (5 * 9)  # once for each set of 10 numbers (except for 18), up to 100
+    total += ((5 * 9) * 10)  # once for each set of 10 numbers (except for 18), up to 100. then repeat for each set of 100
     total += (5 * 100)  # once for each number that starts with "eight hundred" (800-899)
 
     # for each 9 found, add another 4 characters
-    total += (4 * 9)  # once for each set of 10 numbers (except for 19), up to 100
+    total += ((4 * 9) * 10)  # once for each set of 10 numbers (except for 19), up to 100. then repeat for each set of 100
     total += (4 * 100)  # once for each number that starts with "nine hundred" (900-999)
 
 
